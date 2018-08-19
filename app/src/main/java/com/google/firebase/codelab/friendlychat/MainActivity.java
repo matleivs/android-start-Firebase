9add3782532004ed4e071131c6654e6579473cd6
nikbin@itemis.de <nikbin@itemis.de>
2018-08-19 16:46:11 +0200

    Implementing step 11: Remote-configure the length of messages
    With FirebaseRemoteConfig you can configure your app Without the need to deploy any new Code.
    As an example we will configure the max. allowed length for messages with Firebase Remote Config - instead of hardcoding it in the Client (=android code). That way we can update the value of a parameter over the air through the Firebase Console (https://console.firebase.google.com/u/0/project/friendlychat-2807/config)
    
    1. Add a Config Rule in the "Remote Config" section of the Firebase Console
    1a) Add your first parameter -> set the param key to "friendly_message_length" & the param value to 10
    1b) Click "Add paramter"
    1c) Click "Publish Changes"!
    
    in MainActivity.java:
    2. add a FirebaseRemoteConfig instance variable
    3. init it, and
    4. Define & apply firebase remote config settings: we set it to developer mode here
    5. Define & apply default config values: for Use Case that fetching config values from server failed = are not available
    6. add method fetchConfig: tries to fetch the config from server, on Success activates the fetched config, on Failure logs an error
    7. add method applyRetrievedLengthLimit: applies the available length limit to the edittext field. The length limit may be fresh from the server or else from the cached (default?) values.
    
    Test Remote config implementation:
    - change the Remote Config value for the friendly_msg_length param from 10 to 15,
    - (as the fetching is nested in the onCreate method atm:) in the running app, click in Options menu "Fetch fresh config" OR reopen the app
    - confirm that the changes from server apply and the char limit in the message edittext field is now 15.
    
    NOTE: Fresh config requests (requests from Remote Config server) are limited to 5 per hour. If this limit is exceeded requests will be throttled and cached values will be returned till the end of the hour.

--- app/src/main/java/com/google/firebase/codelab/friendlychat/MainActivity.java
+++ app/src/main/java/com/google/firebase/codelab/friendlychat/MainActivity.java
@@ -50,6 +50,8 @@ import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.appindexing.Action;
 import com.google.firebase.appindexing.FirebaseAppIndex;
@@ -63,9 +65,14 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -92,6 +99,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     private FirebaseUser mFirebaseUser;
     private DatabaseReference mFirebaseDatabaseReference;
     private FirebaseRecyclerAdapter<FriendlyMessage, MessageViewHolder> mFirebaseRecyclerAdapter;
+    private FirebaseRemoteConfig mFirebaseRemoteConfig;
 
     // UI
     private Button mSendButton;
@@ -151,6 +159,27 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         mLinearLayoutManager = new LinearLayoutManager(this);
         mLinearLayoutManager.setStackFromEnd(true);
         mMessageRecyclerView.setLayoutManager(mLinearLayoutManager);
+
+        // Initialize the Firebase Remote Config:
+        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+
+        // Define Firebase Remote Config Settings:
+        FirebaseRemoteConfigSettings firebaseRemoteConfigSettings
+                = new FirebaseRemoteConfigSettings.Builder()
+                .setDeveloperModeEnabled(true)
+                .build();
+
+        // Define default config values. Defaults are used when config values are not available,
+        // e.g. if an error occurred on fetching values from the server.
+        Map<String, Object> defaultConfigMap = new HashMap<>();
+        defaultConfigMap.put("friendly_msg_length", 10L);
+        // Apply config settings and default values:
+        mFirebaseRemoteConfig.setConfigSettings(firebaseRemoteConfigSettings);
+        mFirebaseRemoteConfig.setDefaults(defaultConfigMap);
+
+        // fetch remote config:
+        fetchConfig();
+
 
         // ## This code first adds all existing messages and then listens for new child entries under
         // the messages path in my Firebase Realtime Database; it adds a new element to the UI for each msg.
@@ -312,6 +341,52 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         });
     }
 
+    /**
+     * Fetch the firebase remote config to determine the allowed length of messages.
+     */
+    private void fetchConfig() {
+        long cacheExpiration = 3600; // 1 hour in seconds
+        // If developer mode is enabled: reduce cacheExpiration to 0 so that Each fetch goes to the server.
+        // This should Not be used in Release.
+        if(mFirebaseRemoteConfig.getInfo().getConfigSettings()
+            .isDeveloperModeEnabled()){
+            cacheExpiration = 0;
+        }
+        mFirebaseRemoteConfig.fetch(cacheExpiration)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // Make the fetched config available
+                        // via FirebaseRemoteConfig get<type> calls
+                        mFirebaseRemoteConfig.activateFetched();
+                        Log.w(TAG, "Success fetching config.");
+                        applyRetrievedLengthLimit();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Use case: There has been an error fetching the config:
+                        Log.w(TAG, "Error fetching config: " + e.getMessage());
+                        applyRetrievedLengthLimit();
+                    }
+                });
+
+    }
+
+    /**
+     * Apply retrieved length limit to EditText field.
+     * This result may be either fresh from the server or from cached values.
+     */
+    private void applyRetrievedLengthLimit(){
+        Long friendly_msg_length =
+                mFirebaseRemoteConfig.getLong("friendly_msg_length");
+        mMessageEditText.setFilters(new InputFilter[]{
+                new InputFilter.LengthFilter(friendly_msg_length.intValue())});
+
+        Log.d(TAG, "FM Length is: "+ friendly_msg_length);
+    }
+
     @Override
     public void onStart() {
         super.onStart();
@@ -346,6 +421,9 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
+            case R.id.fresh_config_menu:
+                fetchConfig();
+                return true;
             case R.id.sign_out_menu:
                 mFirebaseAuth.signOut();
                 Auth.GoogleSignInApi.signOut(mGoogleApiClient);
